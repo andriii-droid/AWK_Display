de04b9b0d68af4dca2b0a1c84a297fb0bf8f849c
andriii-droid <andri.andermatt@icloud.com>
2026-03-19 00:19:02 +0100

    bidirectional relation between Players and course

--- player.py
+++ player.py
@@ -1,2 +1,5 @@
 class Player:
-    pass
+    def __init__(self, course, data, name, coach):
+        self.course = course
+        self.course.players.append(self)
+        self.name = name
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ day = ["MI", "FR", "DI", "SA"]
 dat = ["12.3", "23.3", "24.3", "26.3"]
 
 f = Course(executed=ex, types=typ, days=day, dates=dat)
+andri = Player(f, [0,1], "Andri")
 
 print(f.att)
 print(f.getIndicesWhereNotCarried("Exercise"))
