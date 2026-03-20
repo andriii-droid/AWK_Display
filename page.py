db022af47c7d03940402273673485ff0aeffe89e
andriii-droid <andri.andermatt@icloud.com>
2026-03-20 11:26:38 +0100

    dynamicclyy append new pages to menu

--- page.py
+++ page.py
@@ -1,20 +1,17 @@
 from nicegui import ui
 
 class Page:
-    def __init__(self, course_name):
-        self.course_name = course_name
+    def __init__(self, course):
+        self.course = course
 
-    # Remove @classmethod - pass the container as an argument instead
     def show_content(self, container):
-        print(f"DEBUG: Rendering {self.course_name} into {container}")
         container.clear()
         with container:
-            ui.label(f'Kurs: {self.course_name}').classes('text-h4 text-primary')
+            ui.label(f'Kurs: {self.course.name}').classes('text-h4 text-primary')
             ui.label('This should now be visible!')
 
     @staticmethod
     def show_home(container):
-        print(f"DEBUG: Rendering Home into {container}")
         container.clear()
         with container:
             ui.label('TTC Uster Dashboard').classes('text-h3')
--- test.py
+++ test.py
@@ -18,16 +18,29 @@ page_nachwuchs = Page(f)
 
 @ui.page('/')
 def main_page():
-    # 1. Create a LOCAL container for THIS specific browser session
-    content_area = ui.column().classes('w-full items-center border p-4')
-    
-    # 2. Navigation
-    with ui.row().classes('w-full justify-center bg-blue-1 p-2'):
-        # We use a lambda to pass the 'content_area' into the methods
-        ui.button('Dashboard', on_click=lambda: Page.show_home(content_area))
-        ui.button('Nachwuchs', on_click=lambda: page_nachwuchs.show_content(content_area))
+    with ui.header().classes('items-center bg-blue-9 px-4'):
+        ui.label('TTC Uster').classes('text-h6 font-bold text-white')
+        
+        ui.space() # Pushes the following button to the right
+        
+        with ui.button(icon='menu').props('flat'):
+            with ui.menu() as menu:
+                ui.menu_item('Home', on_click=lambda: Page.show_home(content_area))
+                
+        
+    def append_item(page):
+        item_count = len(menu.default_slot.children)
+        # Re-enter the menu context to add a new child
+        with menu:
+            ui.menu_item(f'{page.course.name}', 
+                     on_click=lambda: page.show_content(content_area))
+            
+    append_item(page_nachwuchs)
+        
 
-    # 3. Initial Render
+    content_area = ui.column().classes('w-full items-center p-4 mt-2')
+
     Page.show_home(content_area)
 
 ui.run(dark=True)
+
